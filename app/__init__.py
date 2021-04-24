782c4fb67f0679935735343abe94b4e1be525f89
Ari Schechter <aschechter@Aris-MBP.fios-router.home>
2021-04-23 23:46:25 -0400

    added routes for login/signup

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("signup.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
