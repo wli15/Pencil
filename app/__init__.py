5e0cffdd6f7158ad3b7022535351798b66d5a689
Ari Schechter <aschechter@Aris-MBP.fios-router.home>
2021-04-24 09:51:26 -0400

    testing redirect

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("signup.html")
+    return redirect("/login")
 
 @app.route("/login")
 def login():
