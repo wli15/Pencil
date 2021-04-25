3db2cfb5514b2b0f9f4c122fc8229a3285f17db1
Ari Schechter <aschechter@Aris-MBP.fios-router.home>
2021-04-24 21:19:44 -0400

    added user authentication :

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
+from db_manager import *
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+# creates db tables
+create_tables()
+
 
 @app.route("/")
 def index():
+    # check if user already logged in
+    if 'username' in session:
+        return redirect("/home")
     return redirect("/login")
 
 @app.route("/login")
@@ -13,6 +23,40 @@ def login():
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/signupRequest", methods=["POST"])
+def signupRequst():
+    tempUser = request.form["username"]
+    tempPass = request.form["password"]
+    if (tempUser == "") or (tempPass == ""):
+        return redirect("/signup")
+    registerUser(tempUser, tempPass)
+    return redirect("/home")
+
+@app.route("/loginRequest", methods=["POST"])
+def loginRequest():
+    tempUser = request.form['username']
+    tempPass = request.form['password']
+    loginS, error, user_id = checkLogin(tempUser, tempPass)
+    if loginS:
+        session['username'] = tempUser
+        session['password'] = tempPass
+        session['user_id'] = user_id
+        return redirect("/home")
+    # return an error
+    return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    session.pop('username')
+    session.pop('password')
+    session.pop('user_id')
+    return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
---
+++ app/db_manager.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+DB_FILE = "fruits.db"
+
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
+
+def create_tables():
+
+    command = "CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT);"
+
+    command += "CREATE TABLE IF NOT EXISTS recipes(user_id INT, recipe_name TEXT, recipe_url TEXT, recipe_image_url TEXT, recipe_ingrediants TEXT);"
+
+    c.executescript(command)
+    db.commit()
+
+
+def getUserInfo(username):
+    # pass a username, return correct password and user id
+    command = 'SELECT username, password, id FROM users WHERE username = "{}";'.format(username)
+    info = ()
+    for row in c.execute(command):
+        info += (row[0], row[1], row[2])
+    if info == ():
+        return None
+    return info
+
+
+def registerUser(username, password):
+    # pass a username and password and have them inserted into the db
+    command = 'INSERT INTO users VALUES ("{}", "{}", NULL);'.format(username, password)
+    c.execute(command)
+    db.commit()
+
+
+def checkLogin(username, password):
+    # pass a username and password and return whether 
+    # or not the login was successful as well as the user id and an error if unsuccessful
+    info = getUserInfo(username)
+    if info is None:
+        return (False, "Username not found", None)
+    if username == info[0] and password == info[1]:
+        return (True, "", info[2])
+    return (False, "Username or password incorrect", info[2])
