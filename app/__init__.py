9818f20cf054d638ddb5df67f83266b41237f3a3
Ari Schechter <aschechter@Aris-MBP.fios-router.home>
2021-04-25 10:38:26 -0400

    added error handling and welcome message

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ create_tables()
 @app.route("/")
 def index():
     # check if user already logged in
-    if 'username' in session:
+    if 'user_id' in session:
         return redirect("/home")
     return redirect("/login")
 
@@ -29,7 +29,7 @@ def signup():
 
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", username=session['username'])
 
 
 @app.route("/signupRequest", methods=["POST"])
@@ -37,8 +37,10 @@ def signupRequst():
     tempUser = request.form["username"]
     tempPass = request.form["password"]
     if (tempUser == "") or (tempPass == ""):
-        return redirect("/signup")
+        # return an error
+        return render_template("error.html", error="Input fields cannot be left blank. Please fill both in")
     registerUser(tempUser, tempPass)
+    session['username'] = tempUser
     return redirect("/home")
 
 
@@ -53,7 +55,7 @@ def loginRequest():
         session['user_id'] = user_id
         return redirect("/home")
     # return an error
-    return redirect("/login")
+    return render_template("error.html", error=error)
 
 
 @app.route("/logout")
